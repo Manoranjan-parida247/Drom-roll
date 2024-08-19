0fcf2afbb72ded7928d403f383d7bcae3052b95c
Manoranjan-parida247 <manoranjanparida247@gmail.com>
2024-08-19 11:52:09 +0530

    uploading file and folders

---
+++ index.js
@@ -0,0 +1,56 @@
+const btns = document.querySelectorAll(".drum");
+
+for (let i = 0; i < btns.length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var btnInnerHtml = this.innerHTML;
+    playSound(btnInnerHtml);
+    buttonAnimation(btnInnerHtml);
+    
+  });
+}
+
+document.addEventListener("keydown",function(event){
+    playSound(event.key);
+    buttonAnimation(event.key);
+})
+
+function playSound(key){
+    switch (key) {
+        case "w":
+          let tom1 = new Audio("./sounds/tom-1.mp3");
+          tom1.play();
+          break;
+        case "a":
+          let tom2 = new Audio("./sounds/tom-2.mp3");
+          tom2.play();
+          break;
+        case "s":
+          let tom3 = new Audio("./sounds/tom-3.mp3");
+          tom3.play();
+          break;
+        case "d":
+          let tom4 = new Audio("./sounds/tom-4.mp3");
+          tom4.play();
+          break;
+        case "j":
+          let crash = new Audio("./sounds/crash.mp3");
+          crash.play();
+          break;
+        case "k":
+          let kick = new Audio("./sounds/kick-bass.mp3");
+          kick.play();
+          break;
+        case "l":
+          let snare = new Audio("./sounds/snare.mp3");
+          snare.play();
+          break;
+         default:console.log(key);
+    }
+}
+
+function buttonAnimation(currentKey){
+    document.querySelector("." + currentKey).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("." + currentKey).classList.remove("pressed");
+    },1000);
+}
